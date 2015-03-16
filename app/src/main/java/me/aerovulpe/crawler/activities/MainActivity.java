46406b7d31ff4834b06bc4662a5765f4afddf541
Aaron Nwabuoku <aerisvulpe@gmail.com>
2015-03-16 07:05:23 -0400

    Implemented end slideshow when swiped away.

--- app/src/main/java/me/aerovulpe/crawler/activities/MainActivity.java
+++ app/src/main/java/me/aerovulpe/crawler/activities/MainActivity.java
@@ -126,6 +126,8 @@ public class MainActivity extends BaseActivity implements PhotoManagerActivity, 
                 View decorView = getWindow().getDecorView();
                 // Hide the status bar.
                 int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                if (Build.VERSION.SDK_INT >= 19)
+                    uiOptions = uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE;
                 decorView.setSystemUiVisibility(uiOptions);
                 // Remember that you should never show the action bar if the
                 // status bar is hidden, so hide that too if necessary.
