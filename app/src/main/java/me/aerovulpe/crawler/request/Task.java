3438fbaf30e7e37741cef4a6248cca12183a244f
Aaron Nwabuoku <aerisvulpe@gmail.com>
2015-04-14 09:16:08 -0400

    The times, they are a changing.

--- app/src/main/java/me/aerovulpe/crawler/request/Task.java
+++ app/src/main/java/me/aerovulpe/crawler/request/Task.java
@@ -7,46 +7,41 @@ import android.os.AsyncTask;
 public abstract class Task extends AsyncTask<String, String, Boolean> {
 
     public final String ID;
+    final int mResourceId;
+    private Context mContext;
     private ProgressDialog mProgressDialog;
 
     /* UI Thread */
     public Task(Context context, String id, int resourceId) {
         ID = id;
-        mProgressDialog = new ProgressDialog(context);
-        mProgressDialog.setMessage(context.getResources().getString(resourceId));
+        mContext = context;
+        mResourceId = resourceId;
     }
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
-        if (!mProgressDialog.isShowing())
-            mProgressDialog.show();
+        mProgressDialog = new ProgressDialog(mContext);
+        mProgressDialog.setMessage(mContext.getResources().getString(mResourceId));
+        mProgressDialog.show();
     }
 
     /* UI Thread */
     @Override
     protected void onCancelled(Boolean result) {
-        if (mProgressDialog.isShowing())
-            mProgressDialog.dismiss();
+        dismissDialog();
     }
 
     /* UI Thread */
     @Override
     protected void onPostExecute(Boolean result) {
+        dismissDialog();
+    }
+
+    private void dismissDialog() {
         if (mProgressDialog.isShowing())
             mProgressDialog.dismiss();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Task)) return false;
-        Task task = (Task) o;
-        return ID.equals(task.ID);
-    }
-
-    @Override
-    public int hashCode() {
-        return ID.hashCode();
+        mProgressDialog = null;
+        mContext = null;
     }
 }
